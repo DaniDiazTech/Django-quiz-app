081d212bea94eb327c26ccb2b0b65ff9d9fc122d
Daniel Diaz <danieldiazgonza52@gmail.com>
2022-04-17 20:53:56 -0500

    Fixed staticfiles structure

--- config/settings.py
+++ config/settings.py
@@ -128,11 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles/'
 
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     BASE_DIR / 'static',
     BASE_DIR / 'quizes' /  'static'
 ]
